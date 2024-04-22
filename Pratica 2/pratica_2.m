0d9c857066f14c9de9315407aff1dd4461c10c65
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2024-04-22 12:34:22 -0300

    pratica 2

--- Pratica 2/pratica_2.m
+++ Pratica 2/pratica_2.m
@@ -4,9 +4,60 @@
 % Autores: Victor Hugo Daia Lorenzato e Whilker Henrique Santos Silva
 
 %% Limpar workspcade
-clear all;
 close all;
+clear;
 clc;
 
 %% Ex01
-%% Ex02
+syms a b c d;
+M=[a b; c d];
+
+determinante = det(M);
+inversa = inv(M);
+traco = trace(M);
+
+% Exibe os resultados
+disp('Determinante:');
+disp(determinante);
+
+disp('Inversa:');
+disp(inversa);
+
+disp('Traço:');
+disp(traco);
+
+%% Ex02 A
+
+n = 0:10;
+x_n = sym((-1).^n);
+
+figure;
+stem(x_n);
+title('stem');
+
+figure;
+stairs(x_n);
+title('stairs');
+
+figure;
+bar(x_n);
+title('bar');
+
+%% Ex02 B
+
+n = 0:10;
+x_n = sym(cos(pi.*n/12 + pi/4));
+
+figure;
+stem(x_n);
+title('stem');
+
+figure;
+stairs(x_n);
+title('stairs');
+
+figure;
+bar(x_n);
+title('bar');
+
+%% Ex03
