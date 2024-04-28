2b28266b9c8b949b8f735a35198236e19c20ca70
Victor Hugo Daia Lorenzato <victorhugolorenzato@gmail.com>
2024-04-28 17:24:54 -0300

    Update pratica_2.m
    .

--- Pratica 2/pratica_2.m
+++ Pratica 2/pratica_2.m
@@ -62,29 +62,6 @@ title('bar');
 
 %% Ex03
 
-y = zeros(1,3);
-y(1) = 10;
-x = 2;
-
-for n = 2:3
-    y(n) = x + 2*y(n-1);
-end 
-
-disp(y);
 
 %% Ex04
 
-y = zeros(1,10);
-y(1) = 1;
-y(2) = 2;
-
-k = 0;
-for n = 3:10
-    y(n) = k - 2*(k-1) + y(n-1) - 0.24*y(n-2);
-    k = k+1;
-end
-
-disp(y);
-
-t = 0:9;
-stem(t,y);
