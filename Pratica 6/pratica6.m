da080e53717123313cd0d9f6c116616245d2d7f3
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2024-04-29 11:00:22 -0300

    .

---
+++ Pratica 6/pratica6.m
@@ -0,0 +1,10 @@
+%% Laboratório de Sistemas Dinâicos
+% Prática 05
+% 29/04/2024
+% Autores: Victor Hugo Daia Lorenzato e Whilker Henrique Santos Silva
+
+%% Limpar workspcade
+close all;
+clear;
+clc;
+%%
