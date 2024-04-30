7af6d0fa0a0cc798da28dce928b88d3b1099d893
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2024-04-29 23:31:43 -0300

    .

--- Pratica 6/pratica6.m
+++ Pratica 6/pratica6.m
@@ -7,4 +7,25 @@
 close all;
 clear;
 clc;
-%%
+%% Ex01 A
+
+R = 100;
+C = 1*10^-6;
+
+s = tf('s');
+G1_S = 1/(R*C*s + 1);
+
+
+printsys(G1_S.num{1}, G1_S.den{1});
+
+%% Ex01 B
+
+step(G1_S);
+hold on;
+
+%% Ex01 C
+
+R = 2*R;
+G1_S = 1/(R*C*s + 1);
+step(G1_S);
+legend('R=100', 'R=200');
